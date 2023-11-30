bf4305579f8ea8803739e2c6f115d660226f477c
shtrvv <shatrova.liza.21@gmail.com>
2023-11-30 14:06:42 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
-export const App = () => {
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    <div>App</div>
+  )
+}
+
+export default App
